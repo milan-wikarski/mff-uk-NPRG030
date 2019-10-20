c5e6a8e35a54c4b86ce9567655dfa4cae7ce58b5
Milan Wikarski <wikarski.milan@creanet.sk>
2019-10-20 09:47:24 +0200

    Changes in p4

--- p4.py
+++ p4.py
@@ -6,7 +6,11 @@ current = []
 numBefore = None
 num = None
 
+count = 0
+
 while (1):
+  count += 1
+
   numBefore = num
   num = int(input())
 
@@ -19,13 +23,13 @@ while (1):
 
     current.append(str(num))
 
-  if (numBefore >= num or num == -1):
+  if (numBefore >= num or num == -1 or count >= 10000):
     if (len(current) > len(maximal)):
       maximal = copy.deepcopy(current)
 
     current = []
 
-  if (num == -1):
+  if (num == -1 or count >= 10000):
     break
 
 print("\n".join(maximal))
